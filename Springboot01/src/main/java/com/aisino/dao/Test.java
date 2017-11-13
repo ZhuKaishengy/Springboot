9a2e5e2d8135ef97484d1788abe3cd593e8c064b
ZhuKaishengy <13109894018@163.com>
2017-11-13 14:13:20 +0800

    测试idea上的git提交

--- Springboot01/src/main/java/com/aisino/dao/Test.java
+++ Springboot01/src/main/java/com/aisino/dao/Test.java
@@ -4,7 +4,14 @@ package com.aisino.dao;
  * Created by zhukaishengy on 2017-11-13.
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello");
+
+    /**
+     * 测试idea上的git提交
+     * @return
+     */
+    public String add(){
+        return "";
     }
+
+
 }
