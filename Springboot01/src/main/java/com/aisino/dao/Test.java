8edd76af3da7e2ebf8cc8e489c71b178c9e4a5c6
ZhuKaishengy <13109894018@163.com>
2017-11-13 13:58:42 +0800

    Initial commit

---
+++ Springboot01/src/main/java/com/aisino/dao/Test.java
@@ -0,0 +1,10 @@
+package com.aisino.dao;
+
+/**
+ * Created by zhukaishengy on 2017-11-13.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
